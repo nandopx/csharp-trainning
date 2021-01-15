8191b1295baa5ffa34807e8c92abe16b67838e75
Jesse Fernando <nandopox@gmail.com>
2021-01-14 23:41:27 -0300

    Created entities Department, WorkLevel, HourContract and Worker

---
+++ trainning/composition1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace composition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
