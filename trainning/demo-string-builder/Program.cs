c28f70f8601c40f63c372f9ec7e47620abb08b6e
Jesse Fernando <nandopox@gmail.com>
2021-01-15 16:19:30 -0300

    Created a new project to demo StringBuilder

---
+++ trainning/demo-string-builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo_string_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
