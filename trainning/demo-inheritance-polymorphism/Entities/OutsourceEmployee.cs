2ef795754b57eb084f416439f8bb6199a503031f
Jesse Fernando <nandopox@gmail.com>
2021-01-24 14:14:54 -0300

    Created the class Employee and subclass OutsourceEmployee

---
+++ trainning/demo-inheritance-polymorphism/Entities/OutsourceEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo_inheritance_polymorphism.Entities
+{
+    class OutsourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourceEmployee ()
+        {
+        }
+
+        public OutsourceEmployee ( string name, int hours, double valuePerHour, double additionalCharge ) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment ()
+        {
+            base.Payment();
+            AdditionalCharge += 1.1 * AdditionalCharge;
+            return AdditionalCharge;
+        }
+    }
+}
