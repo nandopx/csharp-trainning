888ded68156b43f016bd2dd65f82a013a13816f5
Jesse Fernando <nandopox@gmail.com>
2021-01-25 15:37:22 -0300

    Project created

---
+++ trainning/challenge-inheritance-polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace challenge_inheritance_polymorphism
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
