46c56ce3dbe1506c35fa5b7893ebb193b8588c70
Jesse Fernando <nandopox@gmail.com>
2021-02-01 13:08:17 -0300

    Created the main program

--- trainning/challenge-exceptions/Program.cs
+++ trainning/challenge-exceptions/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using challenge_exceptions.Entities;
+using System;
+using System.Globalization;
 
 namespace challenge_exceptions
 {
@@ -6,7 +8,24 @@ namespace challenge_exceptions
     {
         static void Main ( string[] args )
         {
-            
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            var number = int.Parse( Console.ReadLine() );
+            Console.Write("Holder: ");
+            var holder = Console.ReadLine();
+            Console.Write("initial balance: ");
+            var initial = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+            Console.Write("Withdraw limit: ");
+            var limit = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+
+            var account = new Account( number, holder, initial, limit );
+
+            Console.WriteLine();
+            Console.Write("Enter the amount for withdraw: ");
+            var withDraw = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+            account.WithDraw( withDraw );
+            Console.Write("New balance: $ " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
         }
     }
 }
