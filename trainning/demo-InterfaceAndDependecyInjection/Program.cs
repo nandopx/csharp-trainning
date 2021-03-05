b5feeaa30c323d7ac518124b854009410c0c7fd4
Jesse Fernando <nandopox@gmail.com>
2021-03-05 10:05:36 -0300

    Created the project 'demo-interfaceAndDependencyInjection'

---
+++ trainning/demo-InterfaceAndDependecyInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo_InterfaceAndDependecyInjection
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
