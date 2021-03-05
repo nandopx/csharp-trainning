77990b0a9cb0364519c1f31abe1ad347c4292a1b
Jesse Fernando <nandopox@gmail.com>
2021-03-05 19:59:31 -0300

    Finished the challenge

--- trainning/challenge-Interface/Program.cs
+++ trainning/challenge-Interface/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using challenge_Interface.Entities;
+using challenge_Interface.Services;
+using System;
+using System.Globalization;
 
 namespace challenge_Interface
 {
@@ -6,7 +9,25 @@ namespace challenge_Interface
     {
         static void Main ( string[] args )
         {
-            Console.WriteLine( "Hello World!" );
+            Console.WriteLine( "Enter contract data" );
+            Console.Write( "Number: " );
+            var number = int.Parse( Console.ReadLine() );
+            Console.Write( "Date (dd/MM/yyyy): " );
+            var date = DateTime.ParseExact( Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture );
+            Console.Write( "Contract value: " );
+            var value = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+            Console.Write( "Enter number of installments: " );
+            var months = int.Parse( Console.ReadLine() );
+
+            var contract = new Contract( number, date, value );
+            ContractService contractService = new ContractService( new PaypalService() );
+            contractService.ProcessContract( contract, months );
+
+            Console.WriteLine( "Installments:" );
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine( installment );
+            }
         }
     }
 }
---
+++ trainning/challenge-Interface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace challenge_Interface.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee ( double amount );
+        double Interest (double amount, int months);
+    }
+}
